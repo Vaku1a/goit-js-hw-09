126865d0db724f199e37d4197ba2a0c05f335545
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-10-26 10:50:13 +0300

    01-color-switcher-initial

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const selectors = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+};
 
+let intervalId;
+
+
+selectors.btnStart.addEventListener("click", handlerStart);
+selectors.btnStop.addEventListener("click", handlerStop);
+
+function handlerStart(evt) {
+    evt.currentTarget.disabled = true;
+    if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+    }
+};
+
+function changeBgColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+}
+
+function handlerStop() { 
+    selectors.btnStart.removeAttribute("disabled");
+    clearInterval(intervalId);
+    intervalId = null;
+};
