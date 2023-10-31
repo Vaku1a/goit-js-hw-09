d9ff12df13ff7801e9c5298f5a3f88fdfa8ab10c
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-10-31 19:35:26 +0200

    3-tasks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+selectors = {
+  form: document.querySelector('.form'),
 }
+
+selectors.form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+  const delay = selectors.form.elements.delay.value;
+  const step = selectors.form.elements.step.value;
+  const amount = selectors.form.elements.amount.value;
+  
+  const promises = [];
+  promises.push(createPromise(1, delay))
+  if (amount <= 0) {
+    Notiflix.Notify.failure("Please set the amount more than 0");
+    promises = [];
+  } else {
+    promises.push(createPromise(1, delay))
+  }
+  for (let i = 1; i < amount; i += 1) {
+    promises.push(createPromise(i + 1, i * step + delay))
+  }
+
+  Promise.allSettled(promises).then((items) => {
+    items.forEach((item, i) => {
+      setTimeout(() => {
+        if(item.status === "fulfilled"){
+          Notiflix.Notify.success(item.value);
+        } else{
+          Notiflix.Notify.failure(item.reason);
+        }
+      }, i * step + delay); 
+    });
+  });
+}
+
+ 
+function createPromise(position, delay) {
+  return new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    res(`✅ Fulfilled promise ${position} in ${delay}ms`)
+  } else {
+    rej(`❌ Rejected promise ${position} in ${delay}ms`)
+  }
+  })
+};
+
+
