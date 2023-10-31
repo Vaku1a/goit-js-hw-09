211a4fa0cde4f07feb228a08f010741b448a4624
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-10-31 20:09:46 +0200

    change push() to arr

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,8 @@
 import Notiflix from 'notiflix';
 
-selectors = {
+const selectors = {
   form: document.querySelector('.form'),
-}
+};
 
 selectors.form.addEventListener('submit', handlerSubmit);
 
@@ -11,42 +11,39 @@ function handlerSubmit(evt) {
   const delay = selectors.form.elements.delay.value;
   const step = selectors.form.elements.step.value;
   const amount = selectors.form.elements.amount.value;
-  
   const promises = [];
-  promises.push(createPromise(1, delay))
+
   if (amount <= 0) {
-    Notiflix.Notify.failure("Please set the amount more than 0");
+    Notiflix.Notify.failure('Please set the amount more than 0');
     promises = [];
   } else {
-    promises.push(createPromise(1, delay))
-  }
-  for (let i = 1; i < amount; i += 1) {
-    promises.push(createPromise(i + 1, i * step + delay))
+    promises.push(createPromise(1, delay));
   }
 
-  Promise.allSettled(promises).then((items) => {
+  for (let i = 1; i < amount; i += 1) {
+    promises.push(createPromise(i + 1, i * step + delay));
+  }
+
+  Promise.allSettled(promises).then(items => {
     items.forEach((item, i) => {
       setTimeout(() => {
-        if(item.status === "fulfilled"){
+        if (item.status === 'fulfilled') {
           Notiflix.Notify.success(item.value);
-        } else{
+        } else {
           Notiflix.Notify.failure(item.reason);
         }
-      }, i * step + delay); 
+      }, i * step + delay);
     });
   });
 }
 
- 
 function createPromise(position, delay) {
   return new Promise((res, rej) => {
     const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    res(`✅ Fulfilled promise ${position} in ${delay}ms`)
-  } else {
-    rej(`❌ Rejected promise ${position} in ${delay}ms`)
-  }
-  })
-};
-
-
+    if (shouldResolve) {
+      res(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      rej(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  });
+}
