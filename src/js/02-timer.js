136dcab7d9d474aac4652da9cf3c71b90c891202
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-11-01 16:55:04 +0200

    correct timer after review

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,19 @@
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
 const selectors = {
-    input: document.getElementById("datetime-picker"),
-    btn: document.querySelector('button'),
-    days: document.querySelector('[data-days]'),
-    hours: document.querySelector('[data-hours]'),
-    minutes: document.querySelector('[data-minutes]'),
-    seconds: document.querySelector('[data-seconds]'),
-    label: document.querySelectorAll('.label'),
-    value: document.querySelectorAll('.value'),
-    timerBox: document.querySelector('.timer')
-
+  input: document.getElementById('datetime-picker'),
+  btn: document.querySelector('button'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  label: document.querySelectorAll('.label'),
+  value: document.querySelectorAll('.value'),
+  timerBox: document.querySelector('.timer'),
 };
 
 // let counter;
@@ -30,36 +29,31 @@ const selectors = {
 //     element.style.flexDirection = 'column'
 // });
 
-
-
-
-
-
 selectors.btn.disabled = true;
 selectors.btn.addEventListener('click', handlerStart);
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        const currentDate = options.defaultDate;
-        const selectedDate = selectedDates[0];
-        if (currentDate >= selectedDate) {
-            Notiflix.Report.warning(
-                'Warning',
-                'Please choose a date in the future',
-                'Ok',
-            );
-        } else {
-            selectors.btn.removeAttribute("disabled");
-            counter = selectedDate - currentDate;
-            markup = convertMs(counter);
-            console.log(markup)
-            addLeadingZero(markup);
-        }
-    },
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDate = options.defaultDate;
+    let selectedDate = Number(selectedDates[0]);
+    if (currentDate >= selectedDate) {
+      Notiflix.Report.warning(
+        'Warning',
+        'Please choose a date in the future',
+        'Ok'
+      );
+    } else {
+      selectors.btn.removeAttribute('disabled');
+      counter = selectedDate - currentDate;
+      markup = convertMs(counter);
+      console.log(markup);
+      addLeadingZero(markup);
+    }
+  },
 };
 
 const fp = flatpickr(selectors.input, options);
@@ -91,28 +85,25 @@ function convertMs(ms) {
 
 // ----
 
-function addLeadingZero({days, hours, minutes, seconds}) {
-    selectors.days.textContent = `${days}`.padStart(2, '0');
-    selectors.hours.textContent = `${hours}`.padStart(2, '0');
-    selectors.minutes.textContent = `${minutes}`.padStart(2, '0');
-    selectors.seconds.textContent = `${seconds}`.padStart(2, '0');
-
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  selectors.days.textContent = `${days}`.padStart(2, '0');
+  selectors.hours.textContent = `${hours}`.padStart(2, '0');
+  selectors.minutes.textContent = `${minutes}`.padStart(2, '0');
+  selectors.seconds.textContent = `${seconds}`.padStart(2, '0');
 }
 
-
 function handlerStart() {
-    const timerIdInterval = setInterval(() => {
+  const timerIdInterval = setInterval(() => {
     if (counter <= 1000) {
       clearInterval(timerIdInterval);
       Notiflix.Report.success('Timer is finished', 'Nice job!', 'Done!');
       return;
-    } 
-        
+    }
+
     counter -= 1000;
     markup = convertMs(counter);
     addLeadingZero(markup);
-    
-    }, 1000);
+  }, 1000);
 
-    selectors.btn.disabled = true;
+  selectors.btn.disabled = true;
 }
